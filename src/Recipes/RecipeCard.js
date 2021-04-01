fe4c192cfdcbdaf702a84467eedff62fa4551a21
Max Sveshnikov <msveshnikov@gmail.com>
2021-04-01 20:53:37 +0200

    fix

--- src/Recipes/RecipeCard.js
+++ src/Recipes/RecipeCard.js
@@ -4,7 +4,7 @@ import Typography from "@material-ui/core/Typography";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
-import Link from "@material-ui/core/Link";
+// import Link from "@material-ui/core/Link";
 
 const useStyles = makeStyles((theme) => ({
     media: {
