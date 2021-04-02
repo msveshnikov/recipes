cc55110855204f73f60dbf79b1eb59ffbc2b2ea5
Max Sveshnikov <msveshnikov@gmail.com>
2021-04-02 14:26:54 +0200

    styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { Route, Redirect, Switch } from "react-router-dom";
 import RecipesContainer from "./Recipes/RecipeContainer";
 import Recipe from "./Recipes/Recipe";
+import ScrollToTop from "./hoc/ScrollToTop";
 
 function App() {
     return (
-        <Switch>
-            <Route path="/recipe/:id" component={Recipe} />
-            <Route path="/" exact component={RecipesContainer} />
-            <Redirect to="/" />
-        </Switch>
+        <>
+            <ScrollToTop />
+            <Switch>
+                <Route path="/recipe/:id" component={Recipe} />
+                <Route path="/" exact component={RecipesContainer} />
+                <Redirect to="/" />
+            </Switch>
+        </>
     );
 }
 
--- src/Recipes/Recipe.js
+++ src/Recipes/Recipe.js
@@ -2,16 +2,45 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import recipes from "../db/recipes2.json";
 import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "space-around",
+        backgroundColor: theme.palette.background.paper,
+    },
+}));
 
 const Recipe = () => {
+    const classes = useStyles();
     let { id } = useParams();
     let recipe = recipes.find((r) => r.id === parseInt(id));
-    console.log(recipe);
-    console.log(JSON.parse(recipe?.Media).photos);
+    let photos = JSON.parse(recipe?.Media).photos;
     return (
-        <Typography
-            dangerouslySetInnerHTML={{ __html: recipe.Description || JSON.parse(recipe?.Media).photos[0].text_ru }}
-        ></Typography>
+        <Container component="main" maxWidth="md" className={classes.root}>
+            {recipe.isStepPhoto ? (
+                photos.map((p, index) => (
+                    <div key={index}>
+                        <Typography
+                            dangerouslySetInnerHTML={{
+                                __html: p.text_ru,
+                            }}
+                        ></Typography>
+
+                        <img style={{ marginTop: "15px" }} width="100%" src={p.src} alt="" />
+                    </div>
+                ))
+            ) : (
+                <>
+                    <Typography dangerouslySetInnerHTML={{ __html: recipe.Description }}></Typography>
+
+                    <img style={{ marginTop: "15px" }} width="100%" src={photos[0].src} alt="" />
+                </>
+            )}
+        </Container>
     );
 };
 
