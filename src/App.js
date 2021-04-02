d2ca4d900e8602e0cda83c37c1ed51521c38eead
Max Sveshnikov <msveshnikov@gmail.com>
2021-04-02 17:27:22 +0200

    adsense

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Redirect, Switch } from "react-router-dom";
 import RecipesContainer from "./Recipes/RecipeContainer";
 import Recipe from "./Recipes/Recipe";
 import ScrollToTop from "./hoc/ScrollToTop";
+import AdSense from "react-adsense";
 
 function App() {
     return (
@@ -12,6 +13,13 @@ function App() {
                 <Route path="/" exact component={RecipesContainer} />
                 <Redirect to="/" />
             </Switch>
+            <AdSense.Google
+                client="ca-pub-1351673544337847"
+                slot="2591225138"
+                style={{ display: "block" }}
+                layout="in-article"
+                format="fluid"
+            />
         </div>
     );
 }
