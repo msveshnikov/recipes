05df9fb6e327ab2a23e7713e5cda883d0b420d8b
Max Sveshnikov <msveshnikov@gmail.com>
2021-04-02 13:43:46 +0200

    full recipe

--- src/Recipes/Recipe.js
+++ src/Recipes/Recipe.js
@@ -1,10 +1,18 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import recipes from "../db/recipes2.json";
+import Typography from "@material-ui/core/Typography";
 
 const Recipe = () => {
     let { id } = useParams();
-
-    return <h1>{id}</h1>;
+    let recipe = recipes.find((r) => r.id === parseInt(id));
+    console.log(recipe);
+    console.log(JSON.parse(recipe?.Media).photos);
+    return (
+        <Typography
+            dangerouslySetInnerHTML={{ __html: recipe.Description || JSON.parse(recipe?.Media).photos[0].text_ru }}
+        ></Typography>
+    );
 };
 
 export default Recipe;
