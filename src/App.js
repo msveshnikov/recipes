8dfc4c6cc796f260c55059ccc6664cb5757ee5e7
Max Sveshnikov <msveshnikov@gmail.com>
2021-04-02 12:37:50 +0200

    ads

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import RecipesContainer from "./Recipes/RecipeContainer";
+import AdSense from "react-adsense";
 
 function App() {
-    return <RecipesContainer />;
+    return (
+        <>
+            <AdSense.Google
+                client="ca-pub-1351673544337847"
+                slot="3301281933"
+                style={{ display: "block" }}
+                layout="in-article"
+                format="fluid"
+            />
+            <RecipesContainer />;
+        </>
+    );
 }
 
 export default App;
