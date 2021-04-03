ae331708343b873d9fd29b374191c76190f6c0a8
Max Sveshnikov <msveshnikov@gmail.com>
2021-04-03 17:41:17 +0200

    finish

--- src/db/gen.js
+++ src/db/gen.js
@@ -3,11 +3,20 @@ let categories = require("../db/categories.json");
 var fs = require("fs");
 
 categories.forEach((c) => {
-    let jsonData = recipes.filter((o) => o.category_id === c.id || o.subcategory_id === c.id);
+    let jsonData;
+    if (c.ParentCategory_ID === 160) {
+        let keywords = c.keywords.split(",");
+        jsonData = recipes.filter((o) => keywords.some((k) => o.Title.includes(k)));
+    } else {
+        jsonData = recipes.filter((o) => o.category_id === c.id || o.subcategory_id === c.id);
+    }
     fs.writeFile(c.id + ".json", JSON.stringify(jsonData), () => {});
     if (jsonData[0]) {
         const photo = JSON.parse(jsonData[0].Media).photos[0];
         c.src_big = photo.src_big;
+    }
+    if (c.id === 160) {
+        c.src_big = "https://www.researchworld.com/wp-content/uploads/2013/02/multi-country.jpg";
     }
 });
 
