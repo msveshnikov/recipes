233b8354727bd8bee552a9fb14fb50a95c3a025c
Max Sveshnikov <msveshnikov@gmail.com>
2021-04-04 09:00:18 +0200

    ads

--- src/AdSense/FeedAd.js
+++ src/AdSense/FeedAd.js
@@ -3,11 +3,11 @@ import AdSense from "react-adsense";
 const FeedAd = (props) => (
     <AdSense.Google
         client="ca-pub-1351673544337847"
-        slot="8734618337"
-        style={{ display: "block" }}
-        format="fluid"
-        responsive="true"
-        layoutKey="-6h+dk+3x-60+49"
+        slot="3301281933"
+        style={{ display: "inline-block", width: "320px", height: "100px" }}
+        // format="fluid"
+        // responsive="true"
+        // layoutKey="-6h+dk+3x-60+49"
     />
 );
 
--- src/Categories/CategoryContainer.js
+++ src/Categories/CategoryContainer.js
@@ -3,6 +3,7 @@ import Container from "@material-ui/core/Container";
 import { makeStyles } from "@material-ui/core/styles";
 import { useParams } from "react-router-dom";
 import cats from "../db/cats.json";
+import FeedAd from "./../AdSense/FeedAd";
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -24,6 +25,7 @@ const CategoryContainer = () => {
                 .map((c) => (
                     <CategoryCard key={c.id} category={c} />
                 ))}
+            <FeedAd />
         </Container>
     );
 };
