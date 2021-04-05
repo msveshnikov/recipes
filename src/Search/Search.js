3140922e023ba38e310789c78b5b9a62d1917b85
Max Sveshnikov <msveshnikov@gmail.com>
2021-04-05 17:50:41 +0200

    almost

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -1,19 +1,9 @@
 import React from "react";
-import {
-    InstantSearch,
-    Index,
-    Hits,
-    Stats,
-    SearchBox,
-    Pagination,
-    Highlight,
-    Configure,
-} from "react-instantsearch-dom";
-import Link from "@material-ui/core/Link";
-import { Link as RouterLink } from "react-router-dom";
+import { InstantSearch, Index, Hits, Stats, SearchBox, Pagination, Configure } from "react-instantsearch-dom";
 import algoliasearch from "algoliasearch/lite";
 import "instantsearch.css/themes/algolia.css";
 import "./Search.css";
+import RecipeCard from "../Recipes/RecipeCard";
 
 const searchClient = algoliasearch("P4PLXKJXKN", "4eaf7bb47fdd2af21c0896028cf9e0f0");
 
@@ -49,15 +39,5 @@ function Search() {
 export default Search;
 
 function Recipe({ hit }) {
-    const photo = JSON.parse(hit?.Media).photos?.[0];
-    return (
-        <div className="hit-card">
-            <Link component={RouterLink} to={"/recipe/" + hit.subcategory_id + "/" + hit.id}>
-                <img className="hit-image" src={photo.src_big} align="left" alt={hit.Title} />
-                <h1 className="hit-name">
-                    <Highlight attribute="Title" hit={hit} />
-                </h1>
-            </Link>
-        </div>
-    );
+    return <RecipeCard recipe={hit} />;
 }
