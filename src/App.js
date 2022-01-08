fd372d63fec6b2ecdb92ba68226987b2be6ac01b
Max Sveshnikov <msveshnikov@gmail.com>
2022-01-08 11:30:09 +0100

    theme provider

--- src/App.js
+++ src/App.js
@@ -4,20 +4,25 @@ import Recipe from "./Components/Recipes/Recipe";
 import ScrollToTop from "./hoc/ScrollToTop";
 import CategoryContainer from "./Components/Categories/CategoryContainer";
 import Search from "./Components/Search/Search";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme();
 
 function App() {
     return (
-        <BrowserRouter>
-            <ScrollToTop />
-            <Switch>
-                <Route path="/recipe/:category/:id" component={Recipe} />
-                <Route path="/category/:parent" component={CategoryContainer} />
-                <Route path="/recipes/:category" component={RecipesContainer} />
-                <Route path="/search" component={Search} />
-                <Route path="/" exact component={CategoryContainer} />
-                <Redirect to="/" />
-            </Switch>
-        </BrowserRouter>
+        <ThemeProvider theme={theme}>
+            <BrowserRouter>
+                <ScrollToTop />
+                <Switch>
+                    <Route path="/recipe/:category/:id" component={Recipe} />
+                    <Route path="/category/:parent" component={CategoryContainer} />
+                    <Route path="/recipes/:category" component={RecipesContainer} />
+                    <Route path="/search" component={Search} />
+                    <Route path="/" exact component={CategoryContainer} />
+                    <Redirect to="/" />
+                </Switch>
+            </BrowserRouter>
+        </ThemeProvider>
     );
 }
 
